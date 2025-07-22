601e19897c8b9f666a58317b307b0f2d75b280ba
Cybersayak <sayakcse2@gmail.com>
2025-07-22 23:49:56 +0530

    Building Frontend Phase

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Get the file path from the url of the current module 
+const __filename = fileURLToPath(import.meta.url);
+//  Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+// Serves the HTML file from the public directory
+// Tells express to serve static files from the public directory 
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
+
 
